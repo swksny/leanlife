957acd56ce56712e4786c7dadffcfdc97c53449e
sawakishinya <swksn.823@gmail.com>
2023-01-07 23:19:14 +0900

    ステージ漏れ分

--- private/conf/application.ini.php
+++ private/conf/application.ini.php
@@ -1,11 +1,3 @@
 <?php
 
-define('DOCUMENT_ROOT',$_SERVER['DOCUMENT_ROOT']);
-
-// if(($_SERVER['SERVER_NAME']) == 'localhost'){
-	
-// }else{
-
-// }
-
 ?>
--- private/conf/conf.ini.php
+++ private/conf/conf.ini.php
@@ -1,8 +1,12 @@
 <?php
 	class AppConstant {
-		CONST LOCAL = 0;
+		CONST PRODUCTION = 0;
 		CONST STAGING = 1;
-		CONST PRODUCTION = 2;
+		CONST LOCAL = 99;
+		
+		CONST SERVER_NAME_LOCAL = 'localhost';
+		CONST SERVER_NAME_STAGING = 'stg.leanlife.sakura.ne.jp';
+		CONST SERVER_NAME_PRODUCTION = 'leanlife.sakura.ne.jp';
 		public $aaa;
 	}
 ?>
